27756c1f359a2607078e0334a31613fcaac12112
Dan Wahlin <you@example.com>
2021-07-29 14:52:45 -0700

    Change title in About

--- svelte-app/src/About.svelte
+++ svelte-app/src/About.svelte
@@ -2,8 +2,7 @@
   <div class="content-title-group not-found">
     <h2 class="title">Product Wish List</h2>
     <p>
-      This project was created to help represent a fundamental app written with
-      Svelte. The shopping theme is used throughout the app.
+      Reliable Web Summit About Page
     </p>
     <br />
     <h2 class="title">Resources</h2>
